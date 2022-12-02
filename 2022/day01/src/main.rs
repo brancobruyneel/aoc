fff7d8c8652d535a751ddd6ca2ad5e9d86a620e5
Branco Bruyneel <brancobruyneel@flaptop.local>
2022-12-02 16:37:27 +0100

    wippers

---
+++ 2022/day01/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let part_one = "";
+
+    println!("Part one: {}", part_one);
+
+    let part_two = "";
+
+    println!("Part two: {}", part_two);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part_one() {
+        todo!();
+    }
+
+    #[test]
+    fn part_two() {
+        todo!();
+    }
+}
---
+++ new.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+YEAR=$(date "+%Y")
+MONTH=$(date "+%-m")
+DAY=$(date "+%-d")
+
+if [ $MONTH -ne 12 ]; then
+	echo "You're too early, idiot"
+	exit 1
+fi
+
+if ! grep -Eo "^session=[a-z0-9]+$" ./.cookie >/dev/null 2>&1; then
+	echo "No session cookie found"
+	exit 1
+fi
+
+DAY=$(date "+%d")
+
+DIR="$YEAR/day$DAY"
+
+mkdir -p $DIR
+
+cd $DIR
+
+cargo init -q
+
+curl -b "$(cat ../../.cookie)" -s "https://adventofcode.com/$YEAR/day/$DAY/input" >input.txt
+
+cp ../../template/main.rs ./src
