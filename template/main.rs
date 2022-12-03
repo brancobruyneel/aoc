4c2df8000910d0b9d14ac47af09625f830529df4
Branco <branco.bruyneel@gmail.com>
2022-12-03 16:00:50 +0100

    update template

--- template/main.rs
+++ template/main.rs
@@ -1,22 +1,31 @@
+fn part_one(input: &str) -> u32 {
+    0
+}
+
+fn part_two(input: &str) -> u32 {
+    0
+}
+
 fn main() {
-    let part_one = "";
+    let input = include_str!("../input.txt");
 
-    println!("Part one: {}", part_one);
-
-    let part_two = "";
-
-    println!("Part two: {}", part_two);
+    println!("Part one: {}", part_one(input));
+    println!("Part two: {}", part_two(input));
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "";
+
     #[test]
-    fn part_one() {
-        todo!();
+    fn part_one_works() {
+        assert_eq!(part_one(EXAMPLE), 0);
     }
 
     #[test]
-    fn part_two() {
-        todo!();
+    fn part_two_works() {
+        assert_eq!(part_two(EXAMPLE), 0);
     }
 }
